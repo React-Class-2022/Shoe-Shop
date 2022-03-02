2549f11891880e887aa26896c5caccc671850454
Badare450 <badarepkiemute2014@gmail.com>
2022-03-01 22:44:10 -0800

    display array of strings

--- shoe-shop/src/App.js
+++ shoe-shop/src/App.js
@@ -1,10 +1,16 @@
 import Product from '../src/components/products/product'
-const data = "i am a data";
+const data =
+ [
+ "i am a data 1",
+"i am a data 2",
+"i am a data 3",
+"i am a data 4",
+];
 const App=()=>
 {
     return(
       <div >
-             <h1>{data}</h1>
+            {data.map( data => <h1>{data}</h1>)}
       </div>
     );
 }
