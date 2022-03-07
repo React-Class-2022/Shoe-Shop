cde13bb8d5cc863bcc468de06d6b41f4ecfdc3d7
Badare450 <badarepkiemute2014@gmail.com>
2022-03-06 16:46:51 -0800

    Updated data

--- shoe-shop/src/App.js
+++ shoe-shop/src/App.js
@@ -1,16 +1,30 @@
 import Product from '../src/components/products/product'
-const data =
- [
- "i am a data 1",
-"i am a data 2",
-"i am a data 3",
-"i am a data 4",
+
+const data =[
+{
+  name : 'Sneakers',
+  image: 'sneakers.jpg',
+  Price : '$20',
+ description:'This is an Original shoe from Nike for men',
+},
+{
+  name : 'Timberland',
+  image: 'Timberland.jpg',
+  Price : '$120',
+ description:'This is an Original shoe from Nike for men',
+},
+{
+  name : 'Dolce and Gabanas',
+  image: 'D&G.jpg',
+  Price : '$10',
+ description:'This is an Original shoe from addidas for men',
+}
 ];
 const App=()=>
 {
     return(
       <div >
-            {data.map( data => <h1>{data}</h1>)}
+            {data.map( data => {return(<div> <h1>{data.name}</h1><h1><img src = {data.image} /></h1><h1> {data.Price} </h1></div>)})}
       </div>
     );
 }
