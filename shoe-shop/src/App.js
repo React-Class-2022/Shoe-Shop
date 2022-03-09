c99b3ee93a588ebdfcc7359cb9dd7f32a03adf09
Badare450 <badarepkiemute2014@gmail.com>
2022-03-08 21:55:28 -0800

    Created Our Cart

--- shoe-shop/src/App.js
+++ shoe-shop/src/App.js
@@ -1,30 +1,72 @@
 import Product from '../src/components/products/product'
-
-const data =[
+import React,{useState} from 'react';
+import './App.css';
+import { Image1 }   from './logo192.png';
+const product=[
 {
   name : 'Sneakers',
-  image: 'sneakers.jpg',
+  image: "https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._SX325_BO1,204,203,200_.jpg",
+  Price : '$20',
+ description:'This is an Original shoe from Nike for men',
+},
+{
+  name : 'Sneakers',
+  image: "https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._SX325_BO1,204,203,200_.jpg",
+  Price : '$20',
+ description:'This is an Original shoe from Nike for men',
+},
+{
+  name : 'Sneakers',
+  image: "https://www.bloomzon.com/public/uploads/all/5RuaAIIW4f3TItrIYTDVSlikS1jJjMVmrJtskg4Q.jpg",
+  Price : '$20',
+ description:'This is an Original shoe from Nike for men',
+},
+{
+  name : 'Sneakers',
+  image: "https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._SX325_BO1,204,203,200_.jpg",
   Price : '$20',
  description:'This is an Original shoe from Nike for men',
 },
 {
   name : 'Timberland',
-  image: 'Timberland.jpg',
+  image: "https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._SX325_BO1,204,203,200_.jpg",
   Price : '$120',
  description:'This is an Original shoe from Nike for men',
 },
 {
   name : 'Dolce and Gabanas',
-  image: 'D&G.jpg',
+  image: "https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._SX325_BO1,204,203,200_.jpg",
   Price : '$10',
  description:'This is an Original shoe from addidas for men',
 }
 ];
+
+
 const App=()=>
 {
+  const [cart,setCart] = useState(0);
+
+   const AddtoCart =()=>
+   {
+     return(
+       setCart(cart + 1)
+     );
+   }
     return(
       <div >
-            {data.map( data => {return(<div> <h1>{data.name}</h1><h1><img src = {data.image} /></h1><h1> {data.Price} </h1></div>)})}
+        <header>
+           <div className = "head-wrap">
+             <div className ="ShopTitle">
+               <h1>Shoe-Shop</h1>
+             </div>
+            
+             <div className ="Cart">
+               <h1>Cart ({cart})</h1>
+             </div>
+             </div>
+        </header>
+       
+    {product.map( data => {return(<div> <h1>{data.name}</h1><img src = {data.image} width ="100px" height ="80px" alt="BigCo Inc. logo" /><h1> {data.Price} </h1><button onClick ={AddtoCart}>Add</button></div>)})}
       </div>
     );
 }
